b788063ae73d676cfa96e893597968e5499ab23f
JckXia <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-04-06 21:51:01 -0400

    Completed local login

--- BackEnd/controllers/localLogin.controller.js
+++ BackEnd/controllers/localLogin.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+
 module.exports = {
   login: (req, res) => {
     let username = req.query.username;
@@ -68,6 +69,25 @@ module.exports = {
         });
       });
 
+    });
+  },
+  getUserInfoAuth:(req,res)=>{
+    let token=req.headers['x-access-token'];
+    if(!token) {
+      return res.status(401).send({
+        auth:false,message:'No token provided!'
+      })
+    }
+    jwt.verify(token,'secret',function(err,decoded){
+       if(err){
+         return res.status(500).send({auth:false,message:'Failed to authetnicate token'});
+       }
+       User.findOne({_id:decoded.id},(err,user)=>{
+         if(err){
+           throw err;
+         }
+         res.status(200).send(user);
+       })
     });
   }
 }
--- BackEnd/routes/auth.route.js
+++ BackEnd/routes/auth.route.js
@@ -2,19 +2,31 @@ const express=require('express');
 const router=express.Router();
 const passport=require('passport');
 const login_controller=require('../controllers/localLogin.controller.js');
-
-router.get('/auth/github',passport.authenticate('github'),function(req,res){
-
-});
+const jwt=require('jsonwebtoken');
+const ensureAuthenticated=require('../util/authenticator');
+//Github authentication routes ----------------------------------------------- //
+router.get('/auth/github',passport.authenticate('github'),function(req,res){});
 
 router.get('/auth/github/callback',passport.authenticate('github'),(req,res)=>{
   res.redirect('/');
 });
 
+router.get('/auth/github/logout',(req,res)=>{
 
+  req.logout();
+  res.redirect('/');
+});
+
+// ----------------------------------------------------------------------------- //
+
+
+//Local authentication routes -------------------------------------------------- //
 router.post('/auth/local/register',login_controller.register);
-
 router.post('/auth/local/login',login_controller.login);
+// -------------------------------------------------------------------------------//
+
+router.get('/testAuth',login_controller.getUserInfoAuth);
+
 
 
 router.get('/',(req,res)=>{
