50909edb5bc9f832334721e03e1d455f3addefc7
JckXia <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-08-19 19:09:55 -0400

    Reroute front end files separately

---
+++ jirafront/src/Components/DisplayUserTask.jsx
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import SuperAgent from 'superagent';
+import Column from './column';
+import ProjectTable from './ProjectTable';
+class DisplayUserTask extends Component{
+
+  returnFilteredData(status){
+   console.log('Check');
+   console.log(this.props);
+   const data=this.props.userData.data.filter(card=>card.cardState==status);
+   return data;
+  }
+
+
+   renderUserData(){
+  //   const apiCallFunct=this.testMakingAPICall;
+
+   const headerArr=['Repo name','Creation date','link to repo','repo creator','Delete'];
+   const repoData=this.props.userData.repos;
+
+     switch(this.props.auth){
+
+       case false:
+         return (
+
+           <ul id="nav-mobile" class="center">
+             <h3>Light, agile solution to manage your github repos</h3>
+           </ul>
+         );
+       case true:
+        if(repoData.length === 0){
+          return (
+            <ul id="nav-mobile" class="center">
+              <h4>Looks like you don't have a repo yet. Lets create some!</h4>
+            </ul>
+          )
+        }
+       return (
+
+         <ProjectTable headers={headerArr} rows={repoData}/>
+       );
+     }
+   }
+
+   render(){
+     return(
+       <div>
+      {this.renderUserData()}
+    </div>
+    )
+   }
+};
+
+export default DisplayUserTask;
