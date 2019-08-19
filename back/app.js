ece1a4eea28a25dc766a7463afba5599feb52086
JckXia <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-08-19 19:41:33 -0400

    Fixing buildd

--- back/app.js
+++ back/app.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === 'production') {
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../jirafront/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'jirafront','build', 'index.html'));
   });
 }
 
