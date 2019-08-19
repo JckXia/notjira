db885ccb845f965673e7f9843caef0d8ffe071b0
JckXia <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-08-19 19:36:14 -0400

    Add build folder

--- back/app.js
+++ back/app.js
@@ -57,7 +57,18 @@ app.use('/user',user);
 app.use('/',github);
 app.use('/',auth);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('jirafront/build'));
 
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../jirafront/build', 'index.html'));
+  });
+}
 
 // Listen on port 3000 or process environment port for AWS deployments
 let port=process.env.PORT || 8080;
