47fb012ab8db6c8a2c362a62c2954c7b9f843b9c
JckXia <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-12-30 17:53:13 -0500

    Working resolver function to fetch task Items

--- graphql/resolver/user.js
+++ graphql/resolver/user.js
@@ -1,12 +1,33 @@
-const User = require("../../models/user.model");
+const Repo = require("../../models/repo.model");
+
+const getRepoObjects = async repoData => {
+  console.log("Exected!");
+  const repoIds = repoData.map(repoObject => {
+    return repoObject.id;
+  });
+  try {
+    const repoObjects = await Repo.find({ _id: { $in: repoIds } });
+    return repoObjects.map(repoObject => {
+      return {
+        ...repoObject._doc
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
 module.exports = {
   userInfo: async (args, context) => {
     const userObject = context.user;
+    // getRepoObjects(userObject.repo_lists);
+
     //TODO: Perform another request to get repoLists
     return {
       ...userObject,
       userName: userObject.username,
-      repoLists: userObject.repo_lists
+      gitHubId: userObject.gitHubId,
+      repoLists: getRepoObjects.bind(this, userObject.repo_lists)
     };
   }
 };
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,14 +1,24 @@
 const { buildSchema } = require("graphql");
 
+//TODO Add TaskItem type
 module.exports = buildSchema(`
+
+type TaskItem{
+  _id:ID!
+  task_state:String!
+  taskName:String!
+  taskDesc:String!
+}
 
 type Repo{
  _id:ID!
- name:String!
- webhookurl:String!
- repo_html_url:String!
- date_created:String!
- repo_creator:String!
+ repo_name:String!
+ repo_admin_pk:String!
+ repo_admin_userName:String!
+ repo_admin_name:String!
+ repo_creator_name:String!
+ repo_creator_pk:String!
+ 
 }
 
 type User{
