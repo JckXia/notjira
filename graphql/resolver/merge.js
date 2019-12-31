8e31af5e5333da31a15f24bc4da48b4c460b2282
JckXia <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-12-30 20:16:40 -0500

    Refactored user related code

--- graphql/resolver/merge.js
+++ graphql/resolver/merge.js
@@ -1,5 +1,5 @@
 const Task = require("../../models/task.model");
-
+const Repo = require("../../models/repo.model");
 const getTaskObjects = async taskItemsData => {
   const taskItemIds = taskItemsData.map(taskItem => {
     return taskItem._id;
@@ -16,7 +16,29 @@ const getTaskObjects = async taskItemsData => {
   }
 };
 
+const getRepoObjects = async repoData => {
+  const repoIds = repoData.map(repoObject => {
+    return repoObject.id;
+  });
+  try {
+    const repoObjects = await Repo.find({ _id: { $in: repoIds } });
+    return repoObjects.map(repoObject => {
+      return module.exports.transformRepoObject(repoObject);
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
 module.exports = {
+  transformUserObject: async userObject => {
+    return {
+      ...userObject,
+      userName: userObject.username,
+      gitHubId: userObject.gitHubId,
+      repoLists: getRepoObjects.bind(this, userObject.repo_lists)
+    };
+  },
   transformRepoObject: async repoObject => {
     return {
       ...repoObject._doc,
--- graphql/resolver/user.js
+++ graphql/resolver/user.js
@@ -1,39 +1,4 @@
-const Repo = require("../../models/repo.model");
-const Task = require("../../models/task.model");
-
-const getTaskObjects = async taskItemsData => {
-  const taskItemIds = taskItemsData.map(taskItem => {
-    return taskItem._id;
-  });
-  try {
-    const taskItemObjects = await Task.find({ _id: { $in: taskItemIds } });
-    return taskItemObjects.map(taskItemObject => {
-      return {
-        ...taskItemObject._doc
-      };
-    });
-  } catch (err) {
-    throw err;
-  }
-};
-
-const getRepoObjects = async repoData => {
-  console.log("Exected!");
-  const repoIds = repoData.map(repoObject => {
-    return repoObject.id;
-  });
-  try {
-    const repoObjects = await Repo.find({ _id: { $in: repoIds } });
-    return repoObjects.map(repoObject => {
-      return {
-        ...repoObject._doc,
-        task_items: getTaskObjects.bind(this, repoObject.taskItems)
-      };
-    });
-  } catch (err) {
-    throw err;
-  }
-};
+const { transformUserObject } = require("./merge");
 
 module.exports = {
   userInfo: async (args, context) => {
@@ -41,15 +6,7 @@ module.exports = {
     const requestObject = context.requestBody;
     console.log(requestObject.isAuthenticated());
     if (requestObject.isAuthenticated()) {
-      // getRepoObjects(userObject.repo_lists);
-
-      //TODO: Perform another request to get repoLists
-      return {
-        ...userObject,
-        userName: userObject.username,
-        gitHubId: userObject.gitHubId,
-        repoLists: getRepoObjects.bind(this, userObject.repo_lists)
-      };
+      return transformUserObject(userObject);
     } else {
       throw new Error("403! Unauthenticated!");
     }
